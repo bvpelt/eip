82e5f44ff095c180e9149a25148103d623b00bff
Bart van Pelt <bart.vanpelt@gmail.com>
2017-11-20 21:58:18 +0100

    Added dynamic routes

--- src/main/java/nl/bsoft/eip/EipAutowiredRoutes.java
+++ src/main/java/nl/bsoft/eip/EipAutowiredRoutes.java
@@ -16,14 +16,21 @@ public class EipAutowiredRoutes extends RouteBuilder {
     public void configure() throws Exception {
         logger.info("Configure routes - start");
 
-        from("activemq:queue:MyQueue")
-                .setHeader("Discriminator01", simple("${random(20)}") )
-                .to("log:sample?showHeaders=true");
-
+        /*
         from("timer:bar?fixedRate=true&period=1000")
                 .setHeader("Discriminator", simple("${random(20)}") )
                 .setBody(constant("Hello from Camel"))
                 .to("activemq:queue:MyQueue");
+        */
+
+        from("timer:bar?fixedRate=true&period=1000")
+                .setHeader("mydest", simple("activemq:queue:MyQueue,activemq:queue:MyLog"))
+                .recipientList(header("mydest"))
+                .stopOnException();
+
+        from("activemq:queue:MyQueue")
+                .setHeader("Discriminator01", simple("${random(20)}") )
+                .to("log:sample?showHeaders=true");
 
         from("timer://foo?fixedRate=true&period=5000")
                 .to("bean:myBean?method=someMethodName");
